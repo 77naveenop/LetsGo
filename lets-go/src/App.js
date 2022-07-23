3bc6dd8b23d49d81e493a8b78f75083f6a29b6e1
Navin <naveenpr332@gmail.com>
2022-07-24 01:31:33 +0530

    Update App.js

--- lets-go/src/App.js
+++ lets-go/src/App.js
@@ -1,14 +1,45 @@
 import Home from './Components/Home/Home.jsx';
 import Navbar from './Components/Navbar/Navbar.jsx';
-import Mountains from './Components/Mountains/Mountains.jsx'
+import Mountains from './Components/Mountains/Mountains.jsx';
+import { BrowserRouter, Switch, Route } from "react-router-dom"; 
+import { useState } from 'react';
 import './App.scss';
+import Sidebar from './Components/Sidebar/Sidebar.jsx';
 
 const App =()=>{
+        
+
+  const [sidebar, toggleSidebar] = useState(false)
+
+        const handleToggleSidebar = () => toggleSidebar( value => ! value )
+ 
        return (
         <>
-        <Navbar/>
-        <Home/>
-       <Mountains/>
+  
+     <BrowserRouter>
+     <main>
+      <Navbar handleToggleSidebar={ handleToggleSidebar } />
+
+      <Sidebar sidebar={ sidebar } handleToggleSidebar={ handleToggleSidebar }/>
+         <Switch>
+
+             <Route path="/" component={Home}/>
+                
+             
+             <Route path="/Mountains"  component={Mountains}/>
+                 
+             
+         </Switch>
+     </main>
+     </BrowserRouter>
+
+       
+     
+
+      
+
+
+    
         </>
        )
 }
